146d21986268cb7cd134e512d10dcaecca2b3bf0
prasad.dabhole <prasad.dabhole@IG-M-851.impactguru.net>
2025-07-18 19:23:09 +0530

    Math Class180725

---
+++ src/ab46/MathClass.java
@@ -0,0 +1,22 @@
+package ab46;
+
+public class MathClass {
+
+	public static void main(String[] args) {
+		System.out.println(Math.addExact(200, 100));
+		System.out.println(Math.addExact(7878788455l, 24545451444l));
+		System.out.println(Math.subtractExact(200, 100));
+		System.out.println(Math.subtractExact(7878788455l, 24545451444l));
+		System.out.println(Math.multiplyExact(200, 100));
+		// System.out.println(Math.multiplyExact(7878788455l, 24545451444l));
+		System.out.println(Math.multiplyExact(100, 24545451444l));
+		System.out.println(Math.max(100, 500));
+		System.out.println(Math.max(7878788455l, 24545451444l));
+		System.out.println(Math.max(78.78, 75.58));
+		System.out.println(Math.abs(-78.7899898));
+		System.out.println(Math.min(100, 500));
+		System.out.println(Math.min(7878788455l, 24545451444l));
+		System.out.println(Math.min(78.78, 75.58));
+	}
+
+}
