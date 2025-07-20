a3a7974d5cc6749ceab9348e4e47e85001c97305
prasad.dabhole <prasad.dabhole@IG-M-851.impactguru.net>
2025-07-20 12:33:02 +0530

    Commit dated 20 July 2025

--- src/ab46/Printing1to100.java
+++ src/ab46/Printing1to100.java
@@ -3,7 +3,7 @@ package ab46;
 public class Printing1to100 {
 
 	public static void main(String[] args) {
-		for (int i = 1; i <= 25; i++) {
+		for (int i = 1; i <= 100; i++) {
 			if (i == 19) {
 				continue;
 			}
