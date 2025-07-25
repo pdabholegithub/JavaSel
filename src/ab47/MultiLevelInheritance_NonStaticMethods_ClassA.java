8372476ff46a112d020c23053515f7d09fe67a69
prasad.dabhole <prasad.dabhole@IG-M-851.impactguru.net>
2025-07-25 08:57:22 +0530

    MethodChanges_250725

--- src/ab47/MultiLevelInheritance_NonStaticMethods_ClassA.java
+++ src/ab47/MultiLevelInheritance_NonStaticMethods_ClassA.java
@@ -1,21 +1,21 @@
 package ab47;
 
 class MultiLevelInheritance_ClassD {
-	void addclass4() {
+	void addclassD() {
 		System.out.println("This is class D method");
 	}
 
 }
 
 class MultiLevelInheritance_ClassC extends MultiLevelInheritance_ClassD {
-	void addclass3() {
+	void addclassC() {
 		System.out.println("This is class C method");
 	}
 
 }
 
 class MultiLevelInheritance_ClassB extends MultiLevelInheritance_ClassC {
-	void addclass2() {
+	void addclassB() {
 		System.out.println("This is class B method");
 	}
 
@@ -30,9 +30,9 @@ public class MultiLevelInheritance_NonStaticMethods_ClassA extends MultiLevelInh
 		MultiLevelInheritance_NonStaticMethods_ClassA ca = new MultiLevelInheritance_NonStaticMethods_ClassA();
 
 		ca.addclassA();
-		ca.addclass2();
-		ca.addclass3();
-		ca.addclass4();
+		ca.addclassB();
+		ca.addclassC();
+		ca.addclassD();
 
 	}
 
