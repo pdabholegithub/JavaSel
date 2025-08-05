99c5c3f159b081f6d8cc09a667d023740cd125b7
prasad.dabhole <prasad.dabhole@IG-M-851.impactguru.net>
2025-08-05 23:39:31 +0530

    superkeyword_050825

---
+++ src/ab46/Method_Overriding_Child3.java
@@ -0,0 +1,25 @@
+package ab46;
+
+class Method_Overriding_Parent1 {
+	void login() {
+		System.out.println("Login with Email ID");
+	}
+
+}
+
+public class Method_Overriding_Child3 extends Method_Overriding_Parent1 {
+
+	void login() {
+		super.login();
+		System.out.println("Login with Mobile Number");
+		
+
+	}
+
+	public static void main(String[] args) {
+		Method_Overriding_Child3 c1 = new Method_Overriding_Child3();
+		c1.login();
+
+	}
+
+}
