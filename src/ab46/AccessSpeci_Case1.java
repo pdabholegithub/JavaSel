32c66b86aada6e4836eb890f9ae0732e83c23a6e
pdabholegithub <prasad.dabhole@hotmail.com>
2025-08-09 16:13:37 +0530

    Access_Specifier_090825

---
+++ src/ab46/AccessSpeci_Case1.java
@@ -0,0 +1,18 @@
+package ab46;
+public class AccessSpeci_Case1 {
+	public void add() {
+		System.out.println("This is addition method");
+	}
+	private void sub() {
+		System.out.println("This is subtraction method");
+	}
+	void mul() {
+		System.out.println("This is multiplication method");
+	}
+	protected void div() {
+		System.out.println("This is division method");
+	}
+	public static void main(String[] args) {
+		AccessSpeci_Case1 c1 = new AccessSpeci_Case1();
+		c1.add();		c1.sub(); 		c1.mul(); 		c1.div();
+	}}
