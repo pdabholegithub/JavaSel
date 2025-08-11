6a3d099c9f23a19467ad2c88d45d50bfa28f1670
prasad.dabhole <prasad.dabhole@IG-M-851.impactguru.net>
2025-08-11 21:42:38 +0530

    SubString_110825_2142

---
+++ src/ab46/SubString_Program.java
@@ -0,0 +1,14 @@
+package ab46;
+
+public class SubString_Program {
+
+	public static void main(String[] args) {
+		String a = "Automation";
+		int l1 = a.length();
+		System.out.println(l1);
+		System.out.println(a.substring(5));
+		System.out.println(a.substring(5, 8));
+
+	}
+
+}
