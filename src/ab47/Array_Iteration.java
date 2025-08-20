0c902ba890aa6993cc908f6e5961d8033d7bfb21
prasad.dabhole <prasad.dabhole@IG-M-851.impactguru.net>
2025-08-20 08:01:23 +0530

    Array_Iteration_0801_200825

---
+++ src/ab47/Array_Iteration.java
@@ -0,0 +1,22 @@
+package ab47;
+
+import java.util.Arrays;
+
+public class Array_Iteration {
+
+	public static void main(String[] args) {
+		int array1[] = new int[4];
+		array1[0] = 75;
+		array1[1] = 55;
+		array1[2] = 65;
+		array1[3] = 45;
+		int array2[] = new int[4];
+		for (int i = 0; i < array1.length; i++) {
+			array2[i] = array1[i];
+		}
+		System.out.println("Array 1 data: " + Arrays.toString(array1));
+		System.out.println("Array 2 data: " + Arrays.toString(array2));
+
+	}
+
+}
