5c36130bbbb327bd1ef62619040470217c337065
prasad.dabhole <prasad.dabhole@IG-M-851.impactguru.net>
2025-09-13 22:09:59 +0530

    String_2209_130925

---
+++ src/ab47/StringStringBufferStringBuilder.java
@@ -0,0 +1,20 @@
+package ab47;
+
+public class StringStringBufferStringBuilder {
+
+	public static void main(String[] args) {
+		String name = new String("Selenium");
+		String name1 = name.concat("Testing");
+		System.out.println(name);
+
+		StringBuffer s1 = new StringBuffer("Prasad");
+		s1.append(" Dabhole");
+		System.out.println(s1);
+
+		StringBuilder s2 = new StringBuilder("India");
+		s2.append(" Beautiful Country");
+		System.out.println(s2);
+
+	}
+
+}
